8b482a8cb76f5e7ab5af185aa6dc70bef2982b32
Aryan <aryanebrahimpour@gmail.com>
2018-04-13 12:33:13 +0430

    Init project

--- PhotoFiremark/MainWindow.xaml.cs
+++ PhotoFiremark/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotoFiremark.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace PhotoFiremark
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- PhotoFiremark/Models/NewModel.cs
+++ PhotoFiremark/Models/NewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,4 +11,5 @@ namespace PhotoFiremark.Models
     {
         public string Name { get; set; }
     }
+
 }
